bf7414c9328a9c754913fc6243096c544418cbaf
Jhonatan Alves <jhon_fla@hotmail.com>
2022-01-11 13:40:30 -0300

    Creating Xcode project

---
+++ Movies2You/Movies2You/Movies2YouApp.swift
@@ -0,0 +1,17 @@
+//
+//  Movies2YouApp.swift
+//  Movies2You
+//
+//  Created by Jhonatan Alves on 11/01/22.
+//
+
+import SwiftUI
+
+@main
+struct Movies2YouApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MovieView()
+        }
+    }
+}
---
+++ Movies2You/Movies2You/View/MovieView.swift
@@ -0,0 +1,21 @@
+//
+//  MovieView.swift
+//  Movies2You
+//
+//  Created by Jhonatan Alves on 11/01/22.
+//
+
+import SwiftUI
+
+struct MovieView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct MovieView_Previews: PreviewProvider {
+    static var previews: some View {
+        MovieView()
+    }
+}
