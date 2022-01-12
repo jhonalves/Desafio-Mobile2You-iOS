1d066a279fcbd991b399e97ff6ba00a36cd54443
Jhonatan Alves <jhon_fla@hotmail.com>
2022-01-12 11:35:43 -0300

    Using the MovieViewModel to consume data from the MovieAPI

--- Movies2You/Movies2You/ViewModel/MovieViewModel.swift
+++ Movies2You/Movies2You/ViewModel/MovieViewModel.swift
@@ -7,17 +7,55 @@
 
 import SwiftUI
 
-class MovieViewModel {
-    var movie: Movie?
+class MovieViewModel: ObservableObject {
+    @Published var movie: Movie?
+    var movieAPI = MovieAPI()
     
     init() {
-        getMovie()
+        // gets the given movie when initializing tha ViewModel
+        getMovie(movieID: MovieConstants.movieID)
     }
     
-    func getMovie() {
-        MovieAPI.fetchMovie(movieID: 27205) { (fetchedInfo) in
-            print(fetchedInfo ?? "fails")
+    // gets a movie from the MovieAPI and stores in the movie variable
+    func getMovie(movieID: Int) {
+        self.movieAPI.fetchMovie(movieID: movieID) { (fetchedMovie) in
+            // saves fetched movie if success or nil if fails
+            self.movie = fetchedMovie
+            // if success, gets related movies
+            if fetchedMovie != nil {
+                self.getRelatedMovies(movieID: movieID, modifier: MovieConstants.relatedMoviesModifier)
+            }
         }
-        
+    }
+    
+    // takes related movie id and gets movie data, then appends it to the movie.relatedMovies array
+    func addRelatedMovie(movieID: Int) {
+        self.movieAPI.fetchMovie(movieID: movieID) { (fetchedRelatedMovie) in
+            // if success appends to the movie variable
+            if let relatedMovie = fetchedRelatedMovie {
+                self.movie?.relatedMovies.append(relatedMovie)
+            }
+        }
+    }
+    
+    // gets related movies ids array
+    func getRelatedMovies(movieID: Int, modifier: String) {
+        self.movieAPI.fetchRelatedMovies(movieID: movieID, modifier: modifier) { (fetchedRelatedMovies) in
+            if let list = fetchedRelatedMovies?.relatedMoviesIDs {
+                // for each movie id calls the addRelatedMovie function to
+                // append its movie data to the movie.relatedMovies array
+                for id in list {
+                    self.addRelatedMovie(movieID: id)
+                }
+            }
+        }
+    }
+    
+    // gathering ViewModel constants
+    struct MovieConstants {
+        // corresponds to the "Inception" movie
+        static let movieID = 27205
+        // corresponds to the "Get Similar Movies" function in the TMDB API
+        static let relatedMoviesModifier = "/similar"
     }
 }
