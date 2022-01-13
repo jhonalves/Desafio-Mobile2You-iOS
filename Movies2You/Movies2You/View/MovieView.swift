1bbe378574ead859d7ec28d96441c47193b3fc0b
Jhonatan Alves <jhon_fla@hotmail.com>
2022-01-13 19:38:51 -0300

    Adding back button (just visual)

--- Movies2You/Movies2You/View/MovieView.swift
+++ Movies2You/Movies2You/View/MovieView.swift
@@ -12,25 +12,51 @@ struct MovieView: View {
     @ObservedObject var movieViewModel = MovieViewModel()
     
     var body: some View {
-        ScrollView {
-            if let movie = movieViewModel.movie {
-                VStack(spacing: 0) {
-                    MovieHeaderImage(movie: movie)
-                    MovieHeaderInfo(movie: movie)
+        ZStack {
+            backButton.zIndex(2)
+            ScrollView {
+                if let movie = movieViewModel.movie {
                     VStack(spacing: 0) {
-                        // gets a RelatedMoviesItemView with each related movie's info
-                        ForEach (movie.relatedMovies) { relatedMovie in
-                            RelatedMoviesItemView(movieViewModel: movieViewModel,
-                                                  relatedMovie: relatedMovie,
-                                                  isLastItem: relatedMovie.id == movie.relatedMovies.last?.id)
+                        MovieHeaderImage(movie: movie)
+                        MovieHeaderInfo(movie: movie)
+                        VStack(spacing: 0) {
+                            // gets a RelatedMoviesItemView with each related movie's info
+                            ForEach (movie.relatedMovies) { relatedMovie in
+                                RelatedMoviesItemView(movieViewModel: movieViewModel,
+                                                      relatedMovie: relatedMovie,
+                                                      isLastItem: relatedMovie.id == movie.relatedMovies.last?.id)
+                            }
                         }
+                        .background(.black)
                     }
-                    .background(.black)
                 }
             }
+            .zIndex(1)
+            .background(.black)
+            .ignoresSafeArea()
         }
-        .background(.black)
-        .ignoresSafeArea()
+    }
+    
+    // back button just for visual purposes
+    // no action is executed when pressed
+    var backButton: some View {
+        HStack {
+            VStack {
+                ZStack {
+                    Circle()
+                        .foregroundColor(.black)
+                        .frame(width: 30, height: 30)
+                        .opacity(0.7)
+                    Image(systemName: "chevron.backward")
+                        .foregroundColor(.white)
+                        .imageScale(.large)
+                }
+                .padding(.top, 6)
+                .padding(.leading, 14)
+                Spacer()
+            }
+            Spacer()
+        }
     }
 }
 
