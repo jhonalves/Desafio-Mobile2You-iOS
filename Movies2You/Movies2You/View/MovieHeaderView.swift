37e6c9aac5bf13bfcc928bcbef8b75fe85474c63
Jhonatan Alves <jhon_fla@hotmail.com>
2022-01-13 11:28:25 -0300

    Creating the stretchy header effectwhen scrolling and moving some view elements to separeted files

---
+++ Movies2You/Movies2You/View/MovieHeaderView.swift
@@ -0,0 +1,103 @@
+//
+//  MovieHeaderView.swift
+//  Movies2You
+//
+//  Created by Jhonatan Alves on 13/01/22.
+//
+
+import SwiftUI
+
+// creates a view with the main movie backdrop image, title, likes and views numbers
+struct MovieHeaderView: View {
+    var movie: Movie
+    
+    var body: some View {
+        VStack {
+            ZStack {
+                GeometryReader { geometry in
+                    // gets the image from the movie backdrop URL
+                    AsyncImage(url: URL(string: movie.backdropImageURL)) { phase in
+                        Group {
+                            if let image = phase.image {
+                                image
+                                    .resizable()
+                                    .scaledToFill()
+                                    .clipped()
+                                    .frame(width: geometry.size.width,
+                                           height: StretchyHeaderData.imageHeight(geometry))
+                                    .offset(x: 0, y: StretchyHeaderData.imageOffset(geometry))
+                            }
+                            // if there's a error it shows a black view item
+                            else if phase.error != nil {
+                                Color.black
+                            }
+                            // shows loading element while the image is downloaded
+                            else {
+                                ProgressView()
+                            }
+                        }
+                    }
+                }
+            }
+            // sets the backdrop height to 30% of the screen height
+            .frame(height: UIScreen.main.bounds.height * 0.3)
+            // main movie's info
+            VStack(alignment: .leading) {
+                Text(movie.title)
+                    .bold()
+                    .font(.title)
+                Spacer()
+                HStack(alignment: .center) {
+                    Text(String(movie.likes))
+                    Text("Likes")
+                    Text(String(movie.views))
+                    Text("Views")
+                    Spacer()
+                }
+                .font(.caption)
+            }
+            .foregroundColor(.white)
+            .padding()
+            .background(.black)
+            // shade over the main movie's image
+            .overlay(alignment: .top) {
+                LinearGradient(gradient: Gradient(stops: [
+                        .init(color: .black, location: 0),
+                        .init(color: .clear, location: 1),
+                ]), startPoint: .bottom, endPoint: .top)
+                .frame(height: 100)
+                .offset(x: 0, y: -100)
+            }
+        }
+    }
+}
+
+struct StretchyHeaderData {
+    static func imageOffset(_ geometry: GeometryProxy) -> CGFloat {
+        let offset = geometry.frame(in: .global).minY
+        
+        // when scrolling down
+        if offset > 0 {
+            // image displacement when stretching the image
+            return -offset
+        }
+        // when scrolling up
+        else {
+            // creates the smooth effect in the image when scrolling up
+            return 0.3 * -offset
+        }
+    }
+    
+    static func imageHeight(_ geometry: GeometryProxy) -> CGFloat {
+        let offset = geometry.frame(in: .global).minY
+        var imageHeight = geometry.size.height
+        
+        // when scrolling down
+        if offset > 0 {
+            // adds how much the screen was scrolled to the image height
+            imageHeight = imageHeight + offset
+        }
+        
+        return imageHeight
+    }
+}
---
+++ Movies2You/Movies2You/View/RelatedMoviesItemView.swift
@@ -0,0 +1,83 @@
+//
+//  RelatedMoviesItemView.swift
+//  Movies2You
+//
+//  Created by Jhonatan Alves on 13/01/22.
+//
+
+import SwiftUI
+
+// gets a movie and creates a view with poster image, title, release year and genres
+struct RelatedMoviesItemView: View {
+    var movieViewModel: MovieViewModel
+    var relatedMovie: Movie
+    
+    var body: some View {
+        HStack {
+            // gets the image from the movie poster URL
+            AsyncImage(url: URL(string: relatedMovie.posterImageURL)) { phase in
+                Group {
+                    if let image = phase.image {
+                        image
+                            .resizable()
+                            .scaledToFit()
+                            .clipped()
+                    }
+                    // if there's a error it shows a black view item
+                    else if phase.error != nil {
+                        Color.black
+                    }
+                    // shows loading element while the image is downloaded
+                    else {
+                        ProgressView()
+                    }
+                }
+                .frame(width: 60, height: 100, alignment: .center)
+            }
+            // related movie info
+            HStack {
+                VStack(alignment: .leading, spacing: 6) {
+                    Spacer()
+                    Text(relatedMovie.title)
+                    HStack {
+                        Text(String(relatedMovie.releaseYear))
+                        Text(relatedMovie.genres[0].name)
+                        Spacer()
+                    }
+                    .font(.caption)
+                    Spacer()
+                }
+                .padding(.horizontal, 3)
+                checkMarks
+            }
+            .foregroundColor(.white)
+            Spacer()
+        }
+        .padding(.horizontal, 6)
+    }
+    
+    // set of check marks that are displayed conforming to each related movie data
+    var checkMarks: some View {
+        Group {
+            if relatedMovie.watched {
+                Image(systemName: "checkmark.circle.fill")
+                    .onTapGesture {
+                        // sets both watched and added to false
+                        movieViewModel.toggleWatched(relatedMovieID: relatedMovie.id)
+                    }
+            } else if relatedMovie.added {
+                Image(systemName: "plus.circle.fill")
+                    .onTapGesture {
+                        // sets added to true
+                        movieViewModel.toggleWatched(relatedMovieID: relatedMovie.id)
+                    }
+            } else {
+                Image(systemName: "circle")
+                    .onTapGesture {
+                        // sets added to true
+                        movieViewModel.toggleAdded(relatedMovieID: relatedMovie.id)
+                    }
+            }
+        }
+    }
+}
