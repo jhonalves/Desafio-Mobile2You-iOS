5f0828748d8c3eddab30cf767e758e5ea2459310
Jhonatan Alves <jhon_fla@hotmail.com>
2022-01-13 21:44:55 -0300

    Adding like button with 'flying heart' animation

--- Movies2You/Movies2You/View/MovieHeaderView.swift
+++ Movies2You/Movies2You/View/MovieHeaderView.swift
@@ -46,40 +46,82 @@ struct MovieHeaderImage: View {
 // creates a view with the main movie title, likes and views numbers
 struct MovieHeaderInfo: View {
     var movie: Movie
+    // variable to toggle the heart icon in the interface
+    @State var isLiked: Bool = false
+    // heartOpacity to help the flying heart animation
+    @State var heartOpacity: Double = 0
+    
+    @Namespace private var heartNamespace
     
     var body: some View {
-        // main movie's info
-        VStack(alignment: .leading) {
-            Text(movie.title)
-                .bold()
-                .font(.title)
-            Spacer()
-            HStack(alignment: .center) {
-                HStack(spacing: 0) {
-                    Image(systemName: "heart.fill")
-                    Text(" ")
-                    if movie.likes < 1000 {
-                        // if the number is over 1000, formats it to show in 'thousands K' format
-                        Text(String(format: "%.1fK Likes", movie.likes / 1000)
-                                .replacingOccurrences(of: ".0", with: ""))  // if number ends in .0, removes the dot
-                    } else {
-                        Text(String(format: "%.0f Likes", movie.likes))
+        HStack {
+            // main movie's info
+            VStack(alignment: .leading) {
+                Text(movie.title)
+                    .bold()
+                    .font(.title)
+                Spacer()
+                HStack(alignment: .center) {
+                    HStack(spacing: 0) {
+                        ZStack {
+                            Image(systemName: "heart.fill")
+                            // secondary like icon to help the flying heart animation
+                            if isLiked {
+                                Image(systemName: "heart.fill")
+                                    .transition(AnyTransition.identity)
+                                    .matchedGeometryEffect(id: 1, in: heartNamespace)
+                            }
+                        }
+                        Text(" ")
+                        if movie.likes > 1000 {
+                            // if the number is over 1000, formats it to show in 'thousands K' format
+                            Text(String(format: "%.1fK Likes", movie.likes / 1000)
+                                    .replacingOccurrences(of: ".0", with: ""))  // if number ends in .0, removes the dot
+                        } else {
+                            Text(String(format: "%.0f Likes", movie.likes))
+                        }
                     }
+                    .padding(.trailing, 16)
+                    HStack(spacing: 0) {
+                        Image(systemName: "eye")
+                        Text(" ")
+                        if movie.views > 1000 {
+                            // if the number is over 1000, formats it to show in 'thousands K' format
+                            Text(String(format: "%.1fK Views", movie.views / 1000))
+                        } else {
+                            Text(String(format: "%.0f Views", movie.views))
+                        }
+                    }
+                    Spacer()
                 }
-                Text("   ")
-                HStack(spacing: 0) {
-                    Image(systemName: "eye")
-                    Text(" ")
-                    if movie.views < 1000 {
-                        // if the number is over 1000, formats it to show in 'thousands K' format
-                        Text(String(format: "%.1fK Views", movie.views / 1000))
+                .font(.caption)
+            }
+            Spacer()
+            VStack {
+                // like button just for visual purposes, doesn't reflects on the model
+                ZStack {
+                    if !isLiked {
+                        // shows empty heart if not liked
+                        Image(systemName: "heart").imageScale(.large)
+                            .transition(AnyTransition.scale)
                     } else {
-                        Text(String(format: "%.0f Views", movie.views))
+                            // shows filled heart if liked
+                            Image(systemName: "heart.fill").imageScale(.large)
+                                .transition(AnyTransition.scale)
+                    }
+                    // secondary liked icon to help the flying heart animation
+                    Image(systemName: "heart.fill").imageScale(.large)
+                        .transition(AnyTransition.identity)
+                        .opacity(heartOpacity)
+                        .matchedGeometryEffect(id: 1, in: heartNamespace)
+                }
+                .onTapGesture {
+                    withAnimation(.easeInOut) {
+                        isLiked.toggle()
                     }
                 }
                 Spacer()
             }
-            .font(.caption)
         }
         .foregroundColor(.white)
         .padding()
